7560e1bde6a377186f4dcec1a4388bf0ff878019
João Pedro Sgobero <55161278+Sgobero@users.noreply.github.com>
2022-06-01 15:45:14 -0300

    1/06

---
+++ projeto_integrador-master/app/repositories/UserRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+    require_once __DIR__ . "/../connection/Connection.php";
+
+    class UserRepository {
+
+        private PDO $conn;
+
+        function __construct(){
+
+            $this->conn = Connection::getConnection();
+        }
+
+
+        public function create(){
+
+        }
+
+        public function findAll():array{
+            $query = "SELECT * FROM users";
+            $table = $this->conn->query($query);
+
+            $usuarios = $table->fetchAll(PDO::FETCH_ASSOC);
+
+            return $usuarios;
+
+        }
+
+        public function update(){
+
+        }
+
+        public function delete(){
+
+        }
+    }
